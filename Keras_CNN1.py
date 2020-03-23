a7dd48ecdfe9f4131aa3824687a1740fdd18a032
Nucelles <33731528+Nucelles@users.noreply.github.com>
2020-03-23 15:05:41 +0100

    Add files via upload
     - Working Training of tNN 1

--- Keras_CNN1.py
+++ Keras_CNN1.py
@@ -1,27 +1,58 @@
 import keras
+import numpy as np
+from tqdm import tqdm
+import os
+import cv2
+import pickle
 
-from tensorflow.keras.metrics import MeanIoU
+from random import shuffle
+from tools import getPlateInfo
+from keras.metrics import MeanIoU
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 
 
 
 # PREPARE THE DATASET FOR TRAINING
+imageSize = [1920, 1080, 3]
+path = "C:/Users/Nucelles 3.0/Documents/BICS/BSP S2/Project/DATA/training"
+imageDirectory = os.listdir(path+"/images")
+numberOfImages = len(imageDirectory)
 
-path = "/DATA/Training"
+pairedData = [] # paired x and y, so that we may shuffle
+
+if numberOfImages != len(os.listdir(path+"/images")):
+    print("ERROR: The number of images is not equal to the number of labels")
+    exit()
+
+for currImage in tqdm(range(200)):
+
+    #Prepare the X input
+    imageName = imageDirectory[currImage]
+    image = cv2.imread(path+"/images/"+imageName) / 255
 
 
+    #Prepare the Y input
+    label = imageName[:-3]+"txt"
+    label = getPlateInfo(open(path+"/plates/"+label))[1]
 
+    pairedData.append([image, label])
+
+
+shuffle(pairedData)
+
+x_train = [a[0] for a in pairedData]
+y_train = [a[1] for a in pairedData]
 
 
 
 
 # PREPARE THE CNN1 FOR TRAINING
-# CNN 1 - Detecting the Lincense Plate
+# CNN 1 - Detecting the License Plate
 cnn1 = Sequential(name = "CNN1")
-cnn1.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation="relu"))
+cnn1.add(Conv2D(32, (3, 3), input_shape=(1080, 1920, 3), activation="relu"))
 cnn1.add(MaxPooling2D(pool_size=(2, 2)))
 cnn1.add(Conv2D(64, (3, 3), activation="relu"))
 cnn1.add(MaxPooling2D(pool_size=(2, 2)))
@@ -31,4 +62,9 @@ cnn1.add(Dense(256, activation="relu"))
 cnn1.add(Dense(4, activation="relu"))
 
 cnn1.compile(optimizer='sgd', loss='mse', metrics=['accuracy', MeanIoU(num_classes=1)])
-cnn1.summary()
+print("Compiled!")
+#cnn1.summary()
+
+print("Fitting!")
+cnn1.fit(x = np.array(x_train), y = y_train, epochs = 10, batch_size = 20)
+
